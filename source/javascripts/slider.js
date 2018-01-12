7279a80229a3579946c3ff816a74e755c6868058
mharribey <mathieuharribey@hotmail.fr>
2018-01-12 10:54:36 +0100

    add anim

--- source/javascripts/slider.js
+++ source/javascripts/slider.js
@@ -11,23 +11,65 @@ var array = [profile,projects,index];
 var arrayText = ["profile","projects","index"];
 var array2 = [projDiv, profDiv, helloDiv];
 
+var loading = document.querySelector("#loading");
+
+// CHANGE LA VARIABLE POUR VARIER LE TEMPS POUR LE CHANGEMENT AUTO DES PAGES (en s)
+
+var temps = 8; //soit 8 secones
+
+function load(){
+    var x = 0;
+  setInterval(function(){
+    x+= 0.1;
+    loading.style.width = x+"%";
+    console.log(x);
+  },temps);
+}
+
+function change(item,indexArray){
+  item.classList.add('remove');
+  setTimeout(function(){
+    item.classList.remove('is-ready');
+  },1600);
+  setTimeout(function(){
+    window.location.href = '../'+arrayText[array.indexOf(indexArray)]+'.html';
+  },400);
+}
+
 Array.from(array2).forEach(l=>{
   if(l){
     document.addEventListener('DOMContentLoaded', function() {
+      load();
+
       setTimeout(function(){
         l.classList.add('is-ready');
       },50);
-    }, false);
+    });
+
+    setInterval(function(){
+      var loc = location.pathname;
+      var numbIndex;
+
+      switch (loc) {
+        case "/index.html":
+            numbIndex = 1;
+          break;
+        case "/profile.html":
+            numbIndex = 2;
+          break;
+        case "/projects.html":
+            numbIndex = 0;
+          break;
+        default:
+          numbIndex = 0;
+      }
+      change(l,array[numbIndex]);
+    },temps*1000);
 
     Array.from(array).forEach(p=>{
       p.addEventListener("click",function(){
-        l.classList.add('remove');
-        setTimeout(function(){
-          l.classList.remove('is-ready');
-        },1600);
-        setTimeout(function(){
-          window.location.href = '../'+arrayText[array.indexOf(p)]+'.html';
-        },400);
+        console.log(p);
+        change(l,p);
       });
     });
 
